f19f0802072f7c21da1cabf826e0b8eafaba2157
jerint369 <jerint369@gmail.com>
2023-03-09 17:58:37 -0500

    Login screen and signup working

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,35 +1,57 @@
 import React from "react";
-import { View, Linking } from "react-native";
+import {Linking } from "react-native";
 import {
-  Layout,
-  Button,
+  View,
   Text,
-  Section,
-  SectionContent,
-  useTheme,
-} from "react-native-rapi-ui";
+  TextInput,
+  Button,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Layout,
+} from "react-native";
 
 export default function ({ navigation }) {
-  const { isDarkmode, setTheme } = useTheme();
+  // const { isDarkmode, setTheme } = useTheme();
   return (
-    <Layout>
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          marginHorizontal: 20,
-        }}
-      >
-        <Section>
-          <SectionContent>
-            <Text fontWeight="bold" style={{ textAlign: "center" }}>
-              Welcome To Gym+
-            </Text>
-           
-          </SectionContent>
-        </Section>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("../../assets/logo-colour.png")}
+          style={styles.image}
+        />
       </View>
-    </Layout>
+
+      <Text style={styles.logo}>Welcome to GYM+</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    alignItems: "center",
+    width: "10%",
+    padding: 10,
+  },
+  image: {
+    width: 120,
+    height: 120,
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "black",
+    marginBottom: 40,
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: { width: 0, height: 4 },
+    textShadowRadius: 4,
+  },
+ 
+ 
+});
